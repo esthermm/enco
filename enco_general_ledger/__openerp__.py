3c46b284eecebafd84eb05abd51e5faa0ccb44a1
agaldona <ainaragaldona@avanzosc.es>
2017-01-25 13:34:03 +0100

    [ADD] enco_general_ledger: new module to not print the counterpart column

---
+++ enco_general_ledger/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2017 Ainara Galdona - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Enco - General Ledger",
+    "version": "8.0.1.0.0",
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ainara Galdona <ainaragaldona@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "depends": [
+        "account",
+        ],
+    "data": [
+        "report/report_generalledger.xml",
+        "views/report_view.xml",
+        "wizard/account_report_general_ledger_view.xml"
+        ],
+    "installable": True
+}
